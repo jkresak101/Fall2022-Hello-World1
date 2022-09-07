f5303468f36b1739af17ee15fecc5ed875311748
jkresak101 <jkresak101@gmail.com>
2022-09-07 15:15:56 -0400

    added new stuff

--- Fall2022HelloWorld/Fall2022HelloWorld/Program.cs
+++ Fall2022HelloWorld/Fall2022HelloWorld/Program.cs
@@ -1,3 +1,17 @@
 ﻿// I AM IGNOOOOOOORED :(
 Console.WriteLine("Hello, World!");
 
+string Instructor;
+Instructor = "Gavin";
+Instructor = "Davis";
+var Age = 66;
+
+Console.WriteLine(Age);
+
+var studentCount = 5 + 13;
+Console.WriteLine(studentCount);
+var averageClassSize = studentCount / 4f;
+Console.WriteLine(averageClassSize);
+
+var parsedValue = Int32.Parse("4234");
+Console.WriteLine(parsedValue + 10);
